996f8bb27052ca457103d87931723b74e9aade4f
AlINJFSw <alijanbeincs@gmail.com>
2023-03-12 21:22:48 +0200

    added   comlete

--- src/profile.js
+++ src/profile.js
@@ -1,11 +1,18 @@
 window.onload = async() => {
     const main = document.getElementById("main")
     const token = sessionStorage.getItem("token");
+    const name = document.getElementById("name")
+    const email = document.getElementById("email")
+    const dob = document.getElementById("dob")
+    const save = document.getElementById("save");
     const DATA = await work_pages.postData("http://localhost//hospitals/backend/get_all_user_data.php",null,{
         headers: {
           Authorization: "Bearer " + token,
         },
       });
+      name.value = DATA.main.name
+      email.value = DATA.main.user_email
+      dob.value = DATA.main.dob
       if(DATA.main.user_type == "patient") {
             const patient = document.createElement("div")
             patient.innerHTML = ` <div class="long__input">
@@ -20,6 +27,26 @@ window.onload = async() => {
            </label>
            <input type="text" value = ${DATA.ehr} id="EHR" />`
         main.appendChild(patient)
+        const blood_type = document.getElementById("blood_type").value;
+        const ehr = document.getElementById("ehr").value;
+        const formData = new FormData()
+        formData.append("name", name)
+        formData.append("email", email)
+        formData.append("dob", dob)
+        formData.append("blood_type", blood_type)
+        formData.append("ehr", ehr)
+        save.addEventListener("click" , async() => {
+            if (condition) {
+                const response = await work_pages.postData("http://localhost//hospitals/backend/edit_profile_api.php",formData,{
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          } )
+          console.log(response);
+            }
+          })
+        
+
       }
       else {
         const employee = document.createElement("div")
@@ -42,6 +69,32 @@ window.onload = async() => {
            <input type="text" value = ${DATA.position} id="position" />
            </div>`
         main.appendChild(employee)
+        const ssn = document.getElementById("ssn").value;
+        const position = document.getElementById("position").value;
+        const date_joined = document.getElementById("date_joined").value;
+        const formData = new FormData()
+        formData.append("name", name)
+        formData.append("email", email)
+        formData.append("dob", dob)
+        formData.append("ssn", ssn)
+        formData.append("position", position)
+        formData.append("date_joined", date_joined)
+        
+        
+
+          save.addEventListener("click" , async() => {
+            if (condition) {
+                const response = await work_pages.postData("http://localhost//hospitals/backend/edit_profile_api.php",formData,{
+            headers: {
+              Authorization: "Bearer " + token,
+            },
+          } )
+          console.log(response);
+            }
+          })
       }
+
+
+    
 }
 
